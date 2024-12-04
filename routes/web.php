8f4b2d9c06c6dbac13864b87f6fe32cae9143398
TheWizard <146560714+TheWizard-web@users.noreply.github.com>
2024-12-04 14:12:48 +0200

    Add commit

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\TaskController;
+
 
 
 /*
@@ -18,37 +20,43 @@ use App\Http\Controllers\DashboardController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
-require __DIR__.'/auth.php';
 
-Route::middleware(['auth'])->group(function () {
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-});
+Route::resource('tasks', TaskController::class);
 
-Route::middleware(['auth', 'role:admin'])->group(function ()    {
-    Route::get('/admin-dashboard', [DashboardController::class, 'admin'])->name('admin.dashboard');
-    });
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
-    Route::middleware(['auth', 'role:user'])->group(function () {
-    Route::get('/user-dashboard', [DashboardController::class, 'user'])->name('user.dashboard');
-    });
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
 
-// Route::get('/register', [AuthController::class, 'register'])->name('register');
-// Route::post('/register', [AuthController::class, 'storeRegister']);
+// require __DIR__.'/auth.php';
 
-// Route::get('/login', [AuthController::class, 'login'])->name('login');
-// Route::post('/login', [AuthController::class, 'storeLogin']);
+// Route::middleware(['auth'])->group(function () {
+//     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+// });
 
-// Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+// Route::middleware(['auth', 'role:admin'])->group(function ()    {
+//     Route::get('/admin-dashboard', [DashboardController::class, 'admin'])->name('admin.dashboard');
+//     });
+
+//     Route::middleware(['auth', 'role:user'])->group(function () {
+//     Route::get('/user-dashboard', [DashboardController::class, 'user'])->name('user.dashboard');
+//     });
+
+Route::get('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/register', [AuthController::class, 'storeRegister']);
+
+Route::get('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/login', [AuthController::class, 'storeLogin']);
+
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
